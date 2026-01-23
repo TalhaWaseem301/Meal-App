0712ab69430a99cac0405b71d885232c5cd66644
Sarwar <sarwarofficial029@gmail.com>
2026-01-23 11:25:53 +0500

    all changes upto date

--- lib/features/meals/presentation/screen/meal_screen.dart
+++ lib/features/meals/presentation/screen/meal_screen.dart
@@ -1,51 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-//
-// import '../../../../shared/meals_item.dart';
-// import '../../meal_provider.dart';
-// import 'meal_detail_screen.dart';
-//
-// class MealScreen extends ConsumerWidget {
-//   final String cuisine;
-//   const MealScreen({super.key, required this.cuisine});
-//
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final viewModel = ref.watch(mealViewModelProvider.notifier);
-//     final recipes = viewModel.filteredRecipes;
-//
-//
-//
-//     return Scaffold(
-//       appBar: AppBar(title: Text("$cuisine Recipes")),
-//       body: ListView.builder(
-//         padding: const EdgeInsets.all(16),
-//         itemCount: recipes.length,
-//         itemBuilder: (ctx, index) {
-//           final recipe = recipes[index];
-//           return RecipeCard(
-//             meal: recipe,
-//             onSelectMeal: () {
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(
-//                   builder: (_) => MealDetailScreen(meal: recipe),
-//                 ),
-//               );
-//
-//             },
-//             traits: {
-//               Icons.schedule: '${recipe.prepTimeMinutes ?? 0} min',
-//               Icons.work: recipe.difficulty ?? 'N/A',
-//               Icons.star: '${recipe.rating?.toStringAsFixed(1) ?? '0'}',
-//               // You can add more dynamically if needed
-//             }, onToggleFavorite: () {  },
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -83,7 +35,7 @@ class MealScreen extends ConsumerWidget {
 
       if (filters.filters[MealType.Appetizer] == true &&
           mealTypes.contains("appetizer")) return true;
-
+     // print(mealTypes);
       return false;
     }).toList();
 
